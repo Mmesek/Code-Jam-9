137e34188f40f385402d676a5503f53310d1631c
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-07-25 00:37:45 +0200

    Add way to send initial request upon WS connection

--- src/client/websocket.py
+++ src/client/websocket.py
@@ -3,10 +3,19 @@ import aiohttp
 from src.utils import dispatch
 
 
-async def connection(url: str, registry: dict):
-    """Client websocket connection"""
+async def connection(url: str, registry: dict, *, request: dict = None) -> None:
+    """Client websocket connection
+
+    Args:
+        url: Websocket's URL to connect with
+        registry: Registry to use for function responses
+        request: Initial dictonary send to server upon connecting
+    """
     session = aiohttp.ClientSession()
     ws = await session.ws_connect(url)
+
+    if request:
+        await ws.send_json(request)
 
     try:
         async for msg in ws:
