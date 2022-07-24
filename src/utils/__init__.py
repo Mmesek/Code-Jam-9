a61d5d11cb8a3ad43e2841cb0cb99b1f6ab9b584
Burak Kadir Er <ksenofanex@gmail.com>
2022-07-24 16:01:08 +0300

    Add util functions to package namespace

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,1 @@
-from .models import WebSocket  # noqa: F401
-from .registry import CLIENT_DISPATCH, SERVER_OPCODES  # noqa: F401
-from .utils import dispatch  # noqa: F401
+from src.utils.utils import dispatch, get_ip_address, get_location_info  # noqa: F401
